6a908450dcdd175a58e6e06247746b88e56f6abc
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-09-26 16:56:38 +0100

    Create 3-escape-characters/boy.py
    Added code to demonstrate the use of characters

---
+++ 1-basic/1-output/3-escape-characters/boy.py
@@ -0,0 +1,6 @@
+# Displaying escape characters
+print("\n Display a new line.")
+print("\t Display a tab space.")
+print("\\ Display a back slash.")
+print("\" Display a duoble quote.")
+print("\' Display a single quote.")
