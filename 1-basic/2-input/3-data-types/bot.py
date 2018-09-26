dfda61a10ce453df0bdd2277de4f4de1259a9ea3
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-09-26 18:27:22 +0100

    Create 3-data-types/bot.py
    Added code to demonstrate the use of different data types.

---
+++ 1-basic/2-input/3-data-types/bot.py
@@ -0,0 +1,15 @@
+print("What is your name?")
+name = str(input())
+
+print("How old are you (years)?")
+age = int(input())
+
+print("How much do you weigh (kg)?")
+weight = float(input())
+
+print("How tall are you (m)?")
+height = float(input())
+
+print("Calculating bmi...")
+bmi = weight / (height * height)
+print("Your bmi is", bmi)
