39842dcd0b2b65b0a205ac0c6a5623d3c6d286fa
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-09-26 17:17:40 +0100

    Create 4-ascii-art/bot.py
    Added an example of ascii art.

---
+++ 1-basic/1-output/4-ascii-art/bot.py
@@ -0,0 +1,10 @@
+# Display ascii art
+print("\t       ##")
+print("\t      ####")
+print("\t    []#oo#[]")
+print("\t      ####")
+print("\t       []")
+print("\t[][][][][][][][]")
+print("\t    [][][][]")
+print("\t    [][][][]")
+print("\t    [][][][]")
