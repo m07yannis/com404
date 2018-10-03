1cd17fd5996cca7fdccc9338183e240503959576
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-03 17:24:53 +0100

    Create 5-decision-comparison/bot.py
    Added code for comparison numbers

---
+++ 1-basic/3-decision/5-decision-comparison/bot.py
@@ -0,0 +1,12 @@
+print("Please enter the first number")
+number_1 = int(input())
+
+print("Please enter the second number")
+number_2 = int(input())
+
+if(number_1 > number_2):
+  print("The first number is bigger")
+elif(number_1 < number_2):
+  print("Second number is bigger")
+else:
+  print("Both numbers are equal")
