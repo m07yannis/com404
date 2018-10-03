f0c5fad9cc411a3e6521cc76372c7ed52284d160
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-03 17:11:21 +0100

    Create 4-modulo-operator/bot.py
    Added code for modulo operator

---
+++ 1-basic/3-decision/4-modulo-operator/bot.py
@@ -0,0 +1,17 @@
+# Modulo operator: returns the remainder
+# 5 % 2 returns 1
+# 4 % 2 returns 0
+# 9 % 3 returns 0
+
+# A number is even if it divides by 2 and has no remanider
+
+# Ask the user for a whole number
+print("Please enter a whole number")
+number = int(input())
+
+# Work if the number is even or odd and display  an appropiate message
+if(number == % 2 == 0):
+  print("The number is even")
+
+else:
+  print("The number id odd")
