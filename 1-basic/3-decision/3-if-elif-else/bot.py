47693d467706d6b3598165527bb9bad6c65c00cf
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-03 16:41:54 +0100

    Create 3-decision/3-if-elif-else/bot.py
    Added code to demonstrate the statement of if/else/elif

---
+++ 1-basic/3-decision/3-if-elif-else/bot.py
@@ -0,0 +1,13 @@
+#
+print("Enter a direction")
+print("...w to move up")
+print("...s to move down")
+print("...a to move left")
+print("...d to move right")
+
+direction = input()
+
+#Work out the direction
+if(direction == "w") :
+  print("I am moving up")
+elif()
