b6475ca30556bcf832c43e1787aad950704ecd7a
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-03 16:54:21 +0100

    Create 3-decision/3-if/bot.py
    Added code for if statement

---
+++ 1-basic/3-decision/3-if/bot.py
@@ -0,0 +1,11 @@
+print("Enter one of the following")
+print("...w to move up")
+print("...s to move down")
+print("...a to move left")
+print("...d to move right")
+
+direction = input()
+
+#Work out the direction
+if(direction == "w") :
+  print("I will moving up")
