2215da7eed1c04029956c62ef960cca8f53bd2af
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-10 15:46:36 +0100

    Create 4-repetition/2-for-loop/bot.py
    Added code to display a robot using for loops

---
+++ 1-basic/4-repetition/2-for-loop/bot.py
@@ -0,0 +1,11 @@
+#Dislay a robot using for loop
+NUM_FACES = 3
+
+for count in range(0,NUM_FACES, 1):
+
+ print("~~~~~~~~~")
+ print("#       #")
+ print("# 0   0 #")
+ print("|   ¬   |")
+ print("|  ---  |")
+ print("|_______|")
