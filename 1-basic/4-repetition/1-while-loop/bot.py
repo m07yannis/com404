873525ca3e5829412317a933e543bb141641c802
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-10 15:33:37 +0100

    Create 4-repetition/1-while-loop/bot.py
    Added code to demonstrate the using of while loops

---
+++ 1-basic/4-repetition/1-while-loop/bot.py
@@ -0,0 +1,13 @@
+#Dislay a robot using while loop
+NUM_FACES = 3
+count = 0
+
+while (count < NUM_FACES):
+  print("~~~~~~~~~")
+  print("#       #")
+  print("# O   O #")
+  print("|   V   |")
+  print("|  ---  |")
+  print("|_______|")
+
+  count  = count + 1
