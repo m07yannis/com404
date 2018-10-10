7018c38d0c93c6db0c317f80e9bc9d65ba200ae1
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-10 17:49:42 +0100

    Update bot.py
    Added code for task 4/ Sum

--- 1-basic/4-repetition/4-sum-100/bot.py
+++ 1-basic/4-repetition/4-sum-100/bot.py
@@ -1,4 +1,4 @@
-
+#Display the sum of the first 100 numbers!
 count = 1
 total = 0
 
@@ -6,4 +6,4 @@ while (count <= 100):
   total = total + count
   count = count + 1
 
-print("The sum of the firrst 100 numbers is:", total)
+print("The sum of the first 100 numbers is:", total)
