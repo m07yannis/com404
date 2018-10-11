5e4004be7cadf9c61070dff3256e8d2aa19560c0
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-11 10:31:39 +0100

    Update boy.py
    Added code to display the use of escape characters

--- 1-basic/1-output/3-escape-characters/boy.py
+++ 1-basic/1-output/3-escape-characters/boy.py
@@ -4,3 +4,11 @@ print("\t Display a tab space.")
 print("\\ Display a back slash.")
 print("\" Display a duoble quote.")
 print("\' Display a single quote.")
+
+
+#Dispaly the following "I am Beep !"
+print("\" I am Beep ! \"")
+print("\" Mens Sana In Corpore Sano ! \"")
+print("\'I am Beep ! \'")
+print("\\ I am Beep ! \\")
+print("// I am Beep !//")
