bb4311b7eec9af8d5492a93ade74c1b2c58cf2e1
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-17 15:58:26 +0100

    Create 5-functions/2-leisure/bot.py
    Added code task 2/leisure

---
+++ 1-basic/5-functions/2-leisure/bot.py
@@ -0,0 +1,7 @@
+#Using a function first we define it using "def"
+def activity_user():
+  print("Watch movie")
+  activity = str(input()) # this is our vriable,
+  print("watching a movie sounds like fun",activity)
+
+activity_user() # calling a function
