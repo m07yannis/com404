6fb032e6830856a57305763e480432b96891b88a
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-17 16:20:58 +0100

    Create 5-functions/3-cinderella/bot.py
    Added code task 3/cinderella

---
+++ 1-basic/5-functions/3-cinderella/bot.py
@@ -0,0 +1,14 @@
+def try_glass_slipper_on(person):
+  if(person == "Cinderella"):
+    print("I found my princess!")
+
+  elif(person == "Eldest step syster"):
+   print("You are not my princess. Your feet are too big!")
+
+  elif(person == "Youngest step syster"):
+    print("You are not my princess. Your feet are too small!")
+  
+
+try_glass_slipper_on("Cinderella")
+try_glass_slipper_on("Eldest step syster")
+try_glass_slipper_on("Youngest step syster")
