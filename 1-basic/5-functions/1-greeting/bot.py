b1146fee7ac566609d49f5ca3d1ff26bc4645400
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-10-17 15:46:15 +0100

    Update bot.py
    Added code for defining functions

--- 1-basic/5-functions/1-greeting/bot.py
+++ 1-basic/5-functions/1-greeting/bot.py
@@ -1,8 +1,10 @@
+#define the function
 def greet_user():
-  print("Please enter your name")
-  name = str(input())
-  print("Hello", name)
+  print("Please enter your name") # print the statement between the brackets
+  name = str(input()) # our variable, type of string
+  print("Hello", name) #print the statement "Hello", and the name from user input
 
+#call the function, as many times we like
 greet_user()
 greet_user()
 greet_user()
