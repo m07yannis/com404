a7d05b782a9da8b93702b50a407899e6bda75c65
m07yannis <43613544+m07yannis@users.noreply.github.com>
2018-11-07 17:51:19 +0000

    Update 1-place.py
    Added code GUI

--- 2-guis/2-window-layout/1-place.py
+++ 2-guis/2-window-layout/1-place.py
@@ -24,20 +24,35 @@ class Gui(Tk):
         #add components/widgets
         self.add_heading_label()
         self.add_instruction_label()
-
+        self.add_addres_label()
+        self.add_bottom_button()
         
 
     def add_heading_label(self):   
         # 1. create the component object
         self.heading_label = Label()
-        self.heading_label.place(x=2, y=5)
+        self.heading_label.place(x=100, y=10)
         # 2. style the component
-        self.heading_label.configure(font="Arial 18",
-                                     text="\n \t RECEIVE OUR NEWSLETTER")
+        self.heading_label.configure(font="Arial 16",
+                                     text="RECEIVE OUR NEWSLETTER")
 
     def add_instruction_label(self):
         self.instruction_label = Label()
-        self.instruction_label.place(x=10, y=100)
-        self.instruction_label.configure(font="Arial 8",
+        self.instruction_label.place(x=80, y=80)
+        self.instruction_label.configure(font="Arial 10",
                                   text="Please enter your email below to receive our newsletter")
+        
+    def add_addres_label(self):
+        self.addres_label = Label()
+        self.addres_label.place(x=100, y=200)
+        self.addres_label.configure(font="Arial 12",
+                                         text="Email:")
+
+    def add_bottom_button(self):
+        self.bottom_button = Label()
+        self.bottom_button.place(x=200, y=250)
+        self.bottom_button.configure(font="Arial 12",
+                                     text="Subscribe")
+    
+
         
