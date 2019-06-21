3d40cb1e1a653871e7f52a6ec4a001c84100c3f8
m07yannis <43613544+m07yannis@users.noreply.github.com>
2019-06-21 18:14:53 +0100

    Update logos.py

--- 2-guis/4-images/1-loading/logos.py
+++ 2-guis/4-images/1-loading/logos.py
@@ -11,6 +11,9 @@ class Gui(Tk):
     self.title("TRANSPORT")
     self.configure(bg="#eee",height=300,width=500)
 
+    #add components
+    self.add_heading_label()
+
     #load images/resourse
     self.ambulance_image = PhotoImage(file="ambulance.png")
     self.bike_image = PhotoImage(file="bike.png")
@@ -21,19 +24,28 @@ class Gui(Tk):
     self.add_bike_image_label()
     self.add_plane_image_label()
 
+  def add_heading_label(self):
+    # create the component object
+    self.heading_label = Label()
+    self.heading_label.grid(row=0, column=1)
+    # style the component
+    self.heading_label.configure(font="Arial 18", fg= "black",
+                                     text="Transport")
+
   def add_ambulance_image_label(self):
     self.ambulance_image_label = Label()
-    self.ambulance_image_label.grid(row=0, column=0)
-    self.ambulance_image_label.configure(image=self.ambulance_image, height=60, width=60)
+    self.ambulance_image_label.grid(row=1, column=0)
+    self.ambulance_image_label.configure(image=self.ambulance_image,
+                                         height=60, width=60)
 
   def add_bike_image_label(self):
     self.bike_image_label = Label()
-    self.bike_image_label.grid(row=0, column=1)
+    self.bike_image_label.grid(row=1, column=1)
     self.bike_image_label.configure(image=self.bike_image, height=60, width=60)
 
   def add_plane_image_label(self):
     self.plane_image_label = Label()
-    self.plane_image_label.grid(row=0, column=2)
+    self.plane_image_label.grid(row=1, column=2)
     self.plane_image_label.configure(image=self.plane_image, height=60, width=60)
 
 
